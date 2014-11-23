d2e3f018e4c2194483c0fc0209138c2a40c38ae8
kelvio <kelvio.matias@gmail.com>
2014-11-23 18:44:59 -0200

    Ajustes.

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,8 +2,20 @@ class RankingController < ApplicationController
 
   def index
 
-    sql = "SELECT *, CASE WHEN pontuacao_jogador_a > pontuacao_jogador_b THEN pontuacao_jogador_a ELSE pontuacao_jogador_b END AS pontuacao_vencedor FROM jogos WHERE operacao LIKE '#{ params[:operacao] }' ORDER BY pontuacao_vencedor DESC "
-    @jogos = Jogo.paginate_by_sql(sql, :page => params[:page], :per_page => 20)
+    operacao = params[:operacao]
+    simbolo = '+'
+    if operacao == 'soma'
+      simbolo = '+'
+    elsif operacao == 'subtracao'
+      simbolo = '-'
+    elsif operacao == 'multiplicacao'
+      simbolo = '*'
+    elsif operacao == 'divisao'
+      simbolo = '/'
+    end
+
+    sql = "SELECT *, CASE WHEN pontuacao_jogador_a > pontuacao_jogador_b THEN pontuacao_jogador_a ELSE pontuacao_jogador_b END AS pontuacao_vencedor FROM jogos WHERE operacao LIKE '#{ simbolo }' ORDER BY pontuacao_vencedor DESC "
+    @jogos = Jogo.paginate_by_sql(sql, :page => params[:page], :per_page => 5)
     respond_to do |format|
       format.xml { render :xml => @jogos }
     end
